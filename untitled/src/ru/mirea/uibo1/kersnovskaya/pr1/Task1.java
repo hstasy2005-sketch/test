f7eab313aac7cae9f1205c8dbb9d6e1144646c6b
hstasy2005-sketch <h.stasy2005@gmail.com>
2025-09-18 18:39:31 +0300

    test

---
+++ untitled/src/ru/mirea/uibo1/kersnovskaya/pr1/Task1.java
@@ -0,0 +1,11 @@
+package ru.mirea.uibo1.kersnovskaya.pr1;
+
+public class Task1 {
+    public static void main(String[] args) {
+        System.out.printf("Hello and welcome!");
+
+        for (int i = 1; i <= 5; i++) {
+            System.out.println("i = " + i);
+        }
+    }
+}
